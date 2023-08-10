3293128fde33aa0845ed3700c7b073853ca78f9b
JangWooJin1 <jangwoojin1@gmail.com>
2023-08-10 14:19:13 +0900

    Fix : DB컬럼의 크기 수정

--- myapp/models.py
+++ myapp/models.py
@@ -8,12 +8,12 @@ class Lecture(models.Model):
     lecture_classification = models.CharField(max_length=15, null=True, blank=True)
     lecture_code = models.CharField(max_length=8)
     lecture_number = models.CharField(max_length=5)
-    lecture_name = models.CharField(max_length=50)
-    lecture_professor = models.CharField(max_length=10, null=True, blank=True)
+    lecture_name = models.CharField(max_length=100)
+    lecture_professor = models.CharField(max_length=15, null=True, blank=True)
     lecture_campus = models.CharField(max_length=5, null=True, blank=True)
     lecture_credit = models.IntegerField()
-    lecture_univ = models.CharField(max_length=15)
-    lecture_major = models.CharField(max_length=25)
+    lecture_univ = models.CharField(max_length=15, null=True, blank=True)
+    lecture_major = models.CharField(max_length=50, null=True, blank=True)
     lecture_remark = models.CharField(max_length=100, null=True, blank=True)
 
     class Meta:
@@ -27,7 +27,7 @@ class Lecture(models.Model):
 
 class LectureRoom(models.Model):
     lecture_id = models.ForeignKey(Lecture, on_delete=models.CASCADE)
-    lecture_room = models.CharField(max_length=30)
+    lecture_room = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return f"{self.lecture_id} - {self.lecture_room}"
@@ -35,7 +35,7 @@ class LectureRoom(models.Model):
 
 class LectureTime(models.Model):
     lecture_id = models.ForeignKey(Lecture, on_delete=models.CASCADE)
-    lecture_day = models.CharField(max_length=4)
+    lecture_day = models.CharField(max_length=4, null=True, blank=True)
     lecture_start_time = models.FloatField(null=True, blank=True)
     lecture_end_time = models.FloatField(null=True, blank=True)
 
