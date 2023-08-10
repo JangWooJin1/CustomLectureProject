af3e3d45292da7bd73ce625f26de45e527240f01
JangWooJin1 <jangwoojin1@gmail.com>
2023-08-10 16:32:54 +0900

    Feat : '강의조회' 필터링 버튼 및 검색 부분 구현

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from .models import Lecture
+from django.views import View
+from django.http import JsonResponse
 
 # Create your views here.
+class mainPage(View):
+    template_name = 'mainPage.html'
+
+    def get(self, request):
+        curriculum_list = Lecture.objects.values_list('lecture_curriculum', flat=True).distinct()
+        campus_list = Lecture.objects.values_list('lecture_campus', flat=True).distinct()
+
+        search_label_list = ["교과목", "학수번호", "교원명"]
+        search_value_list = ["lecture_name", "lecture_code", "lecture_professor"]
+        search_zip_list = zip(search_value_list, search_label_list)
+
+        context = {
+            'curriculum_list' : curriculum_list,
+            'campus_list' : campus_list,
+            'search_zip_list' : search_zip_list,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        pass
+
+
+def get_classification_options(request):
+    selected_value = request.GET.get('selected_value')
+    classification_list = Lecture.objects.filter(lecture_curriculum = selected_value).values_list('lecture_classification', flat=True).distinct()
+    options = [{"value" : classification, "label" : classification} for classification in classification_list]
+
+    return JsonResponse(options, safe=False)
+
+
+def get_univ_options(request):
+    univ_list = Lecture.objects.filter(lecture_curriculum = "전공").values_list('lecture_univ', flat=True).distinct()
+    options = [{"value" : univ, "label":univ} for univ in univ_list]
+
+    return JsonResponse(options, safe=False)
+
+
+def get_major_options(request):
+    selected_value = request.GET.get('selected_value')
+    major_list = Lecture.objects.filter(lecture_univ = selected_value).values_list('lecture_major', flat=True).distinct()
+    options = [{"value" : major, "label" : major} for major in major_list]
+
+    return JsonResponse(options, safe=False)
---
+++ static/js/dynamicSelect.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+    $("#mainSelect").change(function () {
+        var selectedValue = $(this).val();
+
+        // AJAX 요청으로 선택에 맞는 옵션들을 가져옵니다.
+        $.ajax({
+            url: get_classification_options_url,  // Django URL 설정에 따라 수정
+            data: { selected_value: selectedValue },
+            success: function (data) {
+                var subSelect = $("#subSelect");
+                subSelect.empty(); // 기존 옵션을 모두 지웁니다.
+                subSelect.append(new Option("--전체--", "all"));
+
+                // 받아온 데이터를 기반으로 새로운 옵션을 추가합니다.
+                for (var i = 0; i < data.length; i++) {
+                    subSelect.append(new Option(data[i].label, data[i].value));
+                }
+            }
+        });
+
+        // '전공'이 선택되면 새로운 <select> 태그를 생성하여 옵션들을 AJAX 요청으로 가져옴
+        if (selectedValue === '전공') {
+            $.ajax({
+                url: get_univ_options_url,  // Django URL 설정에 따라 수정
+                success: function (data) {
+                    var univSelect = $('<select>');
+                    univSelect.attr('id', 'univSelect');  // 새로운 <select> 태그의 id 설정
+                    univSelect.append(new Option("--선택--", "all"));
+                    // 받아온 데이터를 기반으로 새로운 옵션을 추가
+                    for (var i = 0; i < data.length; i++) {
+                        univSelect.append(new Option(data[i].label, data[i].value));
+                    }
+
+                    // 생성한 <select> 태그를 원하는 위치에 추가
+                    $('#selectContainer').append(univSelect);
+
+                    var majorSelect = $('<select>');
+                    majorSelect.attr('id', 'majorSelect');  // 새로운 <select> 태그의 id 설정
+                    majorSelect.append(new Option("--전체--", "all"));
+                    $('#selectContainer').append(majorSelect);
+                }
+            });
+        } else {
+            // '전공'이 아니면 생성한 <select> 태그를 제거
+            $('#univSelect').remove();
+            $('#majorSelect').remove();
+        }
+    });
+
+    $(document).on('change', '#univSelect', function () {
+        var selectedValue = $(this).val();
+        $.ajax({
+            url: get_major_options_url,  // Django URL 설정에 따라 수정
+            data: { selected_value: selectedValue },
+            success: function (data) {
+                var majorSelect = $("#majorSelect");
+                majorSelect.empty(); // 기존 옵션을 모두 지웁니다.
+                majorSelect.append(new Option("--전체--", "all"));
+
+                // 받아온 데이터를 기반으로 새로운 옵션을 추가합니다.
+                for (var i = 0; i < data.length; i++) {
+                    majorSelect.append(new Option(data[i].label, data[i].value));
+                }
+            }
+        });
+    });
+});
